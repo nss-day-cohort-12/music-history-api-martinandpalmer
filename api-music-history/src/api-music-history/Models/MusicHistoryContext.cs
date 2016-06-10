d079fe319c31cb3b3086897db1d0b31bf1b0ef53
Palmer Harrell <palmerharrell@me.com>
2016-06-10 10:53:22 -0500

    Created database context class

---
+++ api-music-history/src/api-music-history/Models/MusicHistoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_music_history.Models
+{
+  public class MusicHistoryContext : DbContext
+  {
+    public MusicHistoryContext(DbContextOptions<MusicHistoryContext> options)
+      : base(options)
+    { }
+
+    public DbSet<Album> Album { get; set; }
+    public DbSet<AppUser> AppUser { get; set; }
+    public DbSet<Track> Track { get; set; }
+  }
+}
