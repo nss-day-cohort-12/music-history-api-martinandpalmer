d9c6dcc240129b3033d2e32a67cb0fd73d0b9c5d
Martin Cross <mncross@gmail.com>
2016-06-13 13:16:54 -0500

    client code to POST user to API upon login

--- frontend-music-history/app/controller/AuthController.js
+++ frontend-music-history/app/controller/AuthController.js
@@ -13,6 +13,24 @@ app.controller('AuthController', [
           if (data.id != null) {
             authFactory.setUser(data);
           }
+          /*
+          POST new user to API. Currently API does not check for existing user. :/
+          */
+          $http({
+            url: 'http://localhost:5000/api/AppUser',
+            method: 'POST',
+            data: JSON.stringify({
+              Username: data.alias,
+              Email: data.email
+            })
+          }).then(
+            resolve => {
+              let AppUser = resolve.data;
+              console.log(`New App User ${AppUser}`);
+            },
+            reject => {
+              console.log("Something went wrong. Perhaps the user is already created");
+            });
           console.log('data', data);
           window.location.assign('/#/');
         })
